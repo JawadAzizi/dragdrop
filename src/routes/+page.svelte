b5b2b930309ea451d640b90bf918e4f878026318
Jawad <jawad.azizi1398@gmail.com>
2023-08-26 19:18:33 +0430

    updata

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import DragWrapper from "$lib/DragWrapper.svelte";
+    import DragZone from "$lib/dragZone.svelte";
+	import DropZone from "$lib/DropZone.svelte";
+
+</script>
+
+
+<div class="home">
+    <DragZone>
+        <DragWrapper>
+            item 1
+        </DragWrapper>
+        <DragWrapper>
+            item 2
+        </DragWrapper>
+        <DragWrapper>
+            item 3
+        </DragWrapper>
+        <DragWrapper>
+            item 4
+        </DragWrapper>
+    </DragZone>
+    <DropZone>
+
+    </DropZone>
+</div>
+
+
+<style>
+    .home{
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+        grid-template-rows: 1fr;
+        gap: 5px;
+    }
+</style>
